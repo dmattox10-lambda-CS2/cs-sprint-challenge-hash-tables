3d5cc46428a9914ba3a4b28caf1317ec06217cfd
Daniel Mattox <dmattox10@gmail.com>
2020-10-02 11:30:00 -0400

    ex4 finished and passing tests

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,15 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
-
+    cache = {}
+    result = []
+    for el in a:
+        if abs(el) not in cache:
+            cache[abs(el)] = 0
+        cache[abs(el)] += 1
+    for el, count in cache.items():
+        if count == 2:
+            result.append(int(el))
     return result
 
 
